dfe9f1ce174407ca655c882e99b7e5381e20312e
Joshua Castor <jcasto3@uic.edu>
2017-06-06 15:35:15 -0500

    Implemented createParticleSystem

--- src/js/particleSystem.js
+++ src/js/particleSystem.js
@@ -30,15 +30,56 @@ var ParticleSystem = function() {
         var geometry = new THREE.CylinderGeometry( radius, radius, height, 32 );
         var material = new THREE.MeshBasicMaterial( {color: 0xffff00, wireframe: true} );
         var cylinder = new THREE.Mesh( geometry, material );
+        cylinder.name = "containment";
 
         // add the containment to the scene
         sceneObject.add(cylinder);
     };
 
     // creates the particle system
+    // based off of https://github.com/mrdoob/three.js/blob/master/examples/webgl_points_billboards.html
     self.createParticleSystem = function() {
 
-        // use self.data to create the particle system
+        let geometry = new THREE.Geometry();
+        let material = new THREE.PointsMaterial({
+            size:  0.1,
+            vertexColors: THREE.VertexColors //use color of each vertex; source: https://codepen.io/antishow/post/three-js-particles
+        });
+
+        var colorScale = (function(){
+            var interpolator = d3.interpolateHsl('#ffffb2', '#bd0026');
+            var scale = d3.scaleLinear()
+                .domain([bounds.concentrationMin,bounds.concentrationMax]) // range of data
+                .range([0, 1]); // range of results
+            return function(value){
+                return interpolator(scale(value));
+            }
+        })(); 
+
+        console.log(bounds.concentrationMin,bounds.concentrationMax);
+
+        for(var i = 0; i < data.length; ++i){
+            //get data point
+            let curDataPoint = data[i];
+            let [x, y, z] = [curDataPoint.X, curDataPoint.Y, curDataPoint.Z];
+            let [u, v, w] = [curDataPoint.U, curDataPoint.V, curDataPoint.W];
+
+            //set datapoint position
+            geometry.vertices.push(new THREE.Vector3(x,y,z));
+
+            var color = colorScale(curDataPoint.concentration);
+            // console.log(color);
+            geometry.colors.push(new THREE.Color(color));
+            // geometry.colors.push(new THREE.Color(Math.random(), Math.random(), Math.random()));
+
+        }
+
+        let particles = new THREE.Points(geometry,material);
+        sceneObject.add(particles);
+
+        //remove containment
+        //based off of https://stackoverflow.com/questions/18357529/threejs-remove-object-from-scene
+        // sceneObject.remove(sceneObject.getObjectByName("containment"));
 
     };
 
@@ -60,18 +101,21 @@ var ParticleSystem = function() {
                 bounds.maxY = Math.max(bounds.maxY || -Infinity, d.Points1);
                 bounds.maxZ = Math.max(bounds.maxY || -Infinity, d.Points2);
 
+                bounds.concentrationMin = Math.min(bounds.concentrationMin || Infinity, Number(d.concentration));
+                bounds.concentrationMax = Math.max(bounds.concentrationMax || -Infinity, Number(d.concentration));
+
                 // add the element to the data collection
                 data.push({
                     // concentration density
                     concentration: Number(d.concentration),
                     // Position
                     X: Number(d.Points0),
-                    Y: Number(d.Points1),
-                    Z: Number(d.Points2),
+                    Z: Number(d.Points1),
+                    Y: Number(d.Points2),
                     // Velocity
-                    U: Number(d.velocity0),
-                    V: Number(d.velocity1),
-                    W: Number(d.velocity2)
+                    U: Number(d.velocity0), //x
+                    W: Number(d.velocity1), //z
+                    V: Number(d.velocity2)  //w
                 });
             })
             // when done loading
@@ -96,6 +140,16 @@ var ParticleSystem = function() {
         // accessor for the particle system
         getParticleSystems : function() {
             return sceneObject;
+        },
+
+        getDataAt: function(y,tolerance){
+            var min =  y - tolerance, max = y + tolerance;
+            var filtered = data.filter(function(point){
+                let curY = point.Y;
+                return curY >= min && curY <= max;
+            });
+
+            return filtered;
         }
     };
 
